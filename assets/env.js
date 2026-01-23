d1fec418660e2c0e7403c6a5b7ffc9b8732002c6
Ceo-Sammarco <noah.kd@gmail.com>
2026-01-22 21:27:35 -0300

    update:  M assets/env.js  M assets/globals.css

--- assets/env.js
+++ assets/env.js
@@ -5,33 +5,34 @@ const env = {
 
 const envtags = {
   endingTag: `<article class="avoid">
-            <t6>Compromisso Elétrica&Art:</t6>
-            <p>Unir técnica, estética, precisão e responsabilidade para entregar um resultado impecável, durável e superior.</p>
-            <tagb>
-              <p>Agradecemos a oportunidade de apresentar esta proposta e estamos à disposição para quaisquer esclarecimentos adicionais.</p>
-            </tagb>
-          </article>
-          <article label="Assinatura e Aprovação">
-             <ui>
-                <header>
-                   <ui>
-                      <t style="font-weight: 700; text-transform: uppercase;">Assinatura e Aprovação</t>
-                   </ui>
-                </header>
-                <content>
-                   <signatures signer cliente>
-                      <signature section>
-                         <content>
-                            <sig-name>Rafael - Elétrica&Art</sig-name>
-                         </content>
-                      </signature>
-                      <signature section>
-                         <content>
-                            <sig-name>Assinatura do Cliente</sig-name> 
-                         </content>
-                      </signature>
-                   </signatures>
-                </content>
-             </ui>
-          </article>`,
+    <content>
+      <t6>Compromisso Elétrica&Art:</t6>
+      <p>Unir técnica, estética, precisão e responsabilidade para entregar um resultado impecável, durável e superior.</p>
+      <tagb>
+        <p>Agradecemos a oportunidade de apresentar esta proposta e estamos à disposição para quaisquer esclarecimentos adicionais.</p>
+      </tagb>
+  </article>
+  <article label="Assinatura e Aprovação">
+    <ui>
+      <header>
+        <ui>
+          <t style="font-weight: 700; text-transform: uppercase;">Assinatura e Aprovação</t>
+        </ui>
+      </header>
+      <content>
+        <signatures signer cliente>
+          <signature section>
+            <content>
+              <sig-name>Rafael - Elétrica&Art</sig-name>
+            </content>
+          </signature>
+          <signature section>
+            <content>
+              <sig-name>Assinatura do Cliente</sig-name> 
+            </content>
+          </signature>
+        </signatures>
+      </content>
+    </ui>
+  </article>`,
 };
