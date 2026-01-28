d9c831ac723302166a8b9b759c9dd9b9a4157de1
Ceo-Sammarco <noah.kd@gmail.com>
2026-01-28 07:35:27 -0300

    update:  M assets/components/AppBar.js ?? pages/budget.html ?? pages/dashboardpaged.html

--- assets/components/AppBar.js
+++ assets/components/AppBar.js
@@ -12,8 +12,8 @@ function initAppBar() {
     { name: "✍️ Novo Orçamento", url: `${prefix}captura.html` },
 
     { name: "📊 Notas", url: `${prefix}notas.html` },
-    /*{ name: "📊 teste 2", url: "#" },
-    { name: "📊 teste 3", url: "#" },*/
+    { name: "📊 dashboard paged", url: `${prefix}dashboardpaged.html` },
+    /*{ name: "📊 teste 3", url: "#" },*/
   ];
 
   const logoText = `
