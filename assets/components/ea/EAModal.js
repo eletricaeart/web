c315e0dbb70a5dd3d2afecf5ab81c7f470aa8d00
Ceo-Sammarco <noah.kd@gmail.com>
2026-02-08 16:18:50 -0300

    update:  M assets/components/ea/EAModal.js  M pages/dashboard.html

--- assets/components/ea/EAModal.js
+++ assets/components/ea/EAModal.js
@@ -41,10 +41,31 @@ class EAModal extends HTMLElement {
           box-shadow: 0 10px 25px rgba(0,0,0,0.2);
           transform: translateY(20px); transition: transform 0.3s ease;
         }
+        .modal > ui {
+          display: block;
+          background: var( --sv-sombra-azul );
+          margin: 0;
+          padding: 0;
+        }
+        .modal > ui > content {
+          display: block;
+          margin: 0;
+          padding: 0;
+          background: #fff;
+          border-radius: 1rem;
+        }
         .active .modal { transform: translateY(0); }
         .header { 
-          padding: 20px; background: var(--sv-sombra-azul, #154a8f); 
+          padding: .5rem; background: var(--sv-sombra-azul, #154a8f); 
           color: white; font-family: 'Montserrat', sans-serif; font-weight: bold;
+        }
+        .header > text {
+          display: block !important;
+          background: var( --sv-sodalita );
+          height: 100%;
+          padding: .6rem;
+          text-align: center;
+          border-radius: 1rem;
         }
         .body { 
           padding: 20px; color: #444; font-family: 'Poppins', sans-serif; line-height: 1.5;
@@ -64,9 +85,15 @@ class EAModal extends HTMLElement {
       </style>
       <div id="overlay" class="overlay">
         <div class="modal">
-          <div class="header" id="title"></div>
-          <div class="body" id="message"></div>
-          <div class="footer" id="footer"></div>
+          <ui>
+            <header class="header" id="">
+              <text id="title"></text>
+            </header>
+            <content>
+              <div class="body" id="message"></div>
+              <div class="footer" id="footer"></div>
+            </content>
+          </ui>
         </div>
       </div>
     `;
@@ -75,7 +102,8 @@ class EAModal extends HTMLElement {
   open(title, message, type, callback) {
     const overlay = this.shadowRoot.getElementById("overlay");
     this.shadowRoot.getElementById("title").innerText = title;
-    this.shadowRoot.getElementById("message").innerText = message;
+    // this.shadowRoot.getElementById("message").innerText = message;
+    this.shadowRoot.getElementById("message").innerHTML = message;
     const footer = this.shadowRoot.getElementById("footer");
 
     footer.innerHTML = ""; // Limpa botões
